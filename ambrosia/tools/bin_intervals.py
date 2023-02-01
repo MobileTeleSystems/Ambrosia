Added theory binary designer - var stabilization asin 1) Added Variance stabilization for binary metrics 2) Add names file with names as variables 3) Add Binary designer handler - CI / theory 4) Fix format in outout

ambrosia/tools/bin_intervals.py
@@ -452,7 +452,7 @@ def get_table_power_on_size_and_delta(
     delta_values: Iterable[float] = None,
     delta_relative_values: Iterable[float] = None,
     amount: int = 10000,
-    confidence_level: float = 0.95,
+    first_errors: float = 0.05,
 ) -> pd.DataFrame:
     """
     Table with power / empirical 1 type error = 1 - coverage for fixed size and effect.
@@ -471,8 +471,8 @@ def get_table_power_on_size_and_delta(
         Relative delta values: delta_relative * p_a = p_b
     amount : int, default : ``10000``
         Amount of generated samples for one n(trials amount), to estimate power
-    confidence_level : float, default : ``0.95``
-        Such value x, that: Pr ( delta in I ) >= x
+    first_errors : floa], default : ``0.05``
+        First type error
 
     Returns
     -------
@@ -499,7 +499,7 @@ def get_table_power_on_size_and_delta(
             "b_success": sample_b,
             "a_trials": trials,
             "b_trials": trials,
-            "confidence_level": confidence_level,
+            "confidence_level": 1 - first_errors,
         }
         conf_interval: types.ManyIntervalType = BinomTwoSampleCI.confidence_interval(**binom_kwargs)
         power: np.ndarray = helper_dir.__helper_calc_empirical_power(conf_interval)
@@ -512,7 +512,7 @@ def get_table_power_on_size_and_delta(
     table_title: str = r"$1 - \beta$: power of criterion, " + (
         r"$p_a-p_b=\Delta$" if delta_values else r"$p_a\delta=p_b$"
     )
-    table = table.style.set_caption(table_title)
+    table = table.applymap(lambda x: f"{x * 100:.2f} %")
     return table
 
 
@@ -654,7 +654,7 @@ def get_table_effect_on_sample_size(
     sample_sizes: Iterable[int] = (100,),
     p_a: float = 0.5,
     amount: int = 10000,
-    delta_type: str = "absolute",
+    delta_type: str = "relative",
 ) -> pd.DataFrame:
     """
     Table for effects with given sample sizes and erros.

ambrosia/tools/theoretical_tools.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Tuple, Optional
 
 import numpy as np
 import pandas as pd
@@ -27,6 +27,13 @@ SECOND_TYPE_ERROR: float = 0.2
 EFFECT_DEFAULT: float = 1.01
 ROUND_DIGITS_TABLE: int = 3
 ROUND_DIGITS_PERCENT: int = 1
+
+
+def make_labels_effects(effects: Iterable[float]) -> List[float]:
+    '''
+    Make row names in table from float effects
+    '''
+    return [f"{np.round((eff - 1) * 100, ROUND_DIGITS_PERCENT)}%" for eff in effects]
 
 
 def get_stats(values: Iterable[float], ddof: int = 1) -> Tuple[float, float]:
@@ -130,7 +137,7 @@ def get_power(mean: float, std: float, sample_size: int, effect: float, alpha: f
     return 1 - second_error
 
 
-def get_table_sample_size(mean, std, effects, first_errors, second_errors):
+def get_table_sample_size(mean, std, effects, first_errors, second_errors, effects_labels=None):
     """
     Create table of sample sizes for different effects and errors.
 
@@ -149,12 +156,18 @@ def get_table_sample_size(mean, std, effects, first_errors, second_errors):
     second_errors : List
         1st and 2nd type errors.
         e.x.: [0.01, 0.05, 0.1]
+    effects_labels : List
+        Labels for effects in table
 
     Returns
     -------
     df_results : pandas df
         Table with minimal sample sizes for each effect and error from input data.
     """
+    
+    first_errors = np.round(first_errors, ROUND_DIGITS_TABLE)
+    second_errors = np.round(second_errors, ROUND_DIGITS_TABLE)
+
     multiindex = pd.MultiIndex.from_tuples([(eff,) for eff in effects], names=["effect"])
     multicols = pd.MultiIndex.from_tuples(
         [(f"({err_one}; {err_two})",) for err_one in first_errors for err_two in second_errors], names=["errors"]
@@ -168,8 +181,13 @@ def get_table_sample_size(mean, std, effects, first_errors, second_errors):
                 df_results.loc[(eff,), (err,)] = get_sample_size(
                     mean=mean, std=std, eff=eff, alpha=first_err, beta=second_err
                 )
+    
+    if effects_labels is None:
+        levels = [[f"{np.round((x - 1) * 100, ROUND_DIGITS_PERCENT)}%" for x in effects]]
+    else:
+        levels = [effects_labels]
     df_results.index = pd.MultiIndex(
-        levels=[[f"{np.round((x - 1) * 100, ROUND_DIGITS_PERCENT)}%" for x in effects]],
+        levels=levels,
         codes=[np.arange(len(effects))],
         names=["effects"],
     )
@@ -315,6 +333,7 @@ def get_power_table(
     effects: Iterable[float],
     first_errors: Iterable[float] = (FIRST_TYPE_ERROR,),
     as_numeric: bool = False,
+    effects_labels: Optional[Iterable[str]] = None
 ) -> pd.DataFrame:
     """
     Create table of power for different sample sizes and effects.
@@ -336,13 +355,18 @@ def get_power_table(
         e.x.: [0.01, 0.05, 0.1]
     as_numeric: bool, default False
         Whether to return a number or a string with percentages
+    effects_labels : List
+        Labels for effects in table
 
     Returns
     -------
     df_results : pandas df
         Table with  sample sizes for each effect and error from input data.
     """
-    effects_str = [str(round((effect - 1) * 100, ROUND_DIGITS_PERCENT)) + "%" for effect in effects]
+    if effects_labels is None:
+        effects_str = [str(round((effect - 1) * 100, ROUND_DIGITS_PERCENT)) + "%" for effect in effects]
+    else:
+        effects_str = effects_labels
     multiindex = pd.MultiIndex.from_tuples(
         [(first_error, effect_str) for first_error in first_errors for effect_str in effects_str],
         names=["First type error", "Effect"],