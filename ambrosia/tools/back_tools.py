cabeef59c25ea992ee79fe8cbc6f06b604a79cf4
Хакимов Артем Валерьевич <artem.khakimov@gmail.com>
2023-03-16 20:33:55 +0300

    Parallelism parameters handling added

--- ambrosia/tools/back_tools.py
+++ ambrosia/tools/back_tools.py
@@ -1,8 +1,11 @@
 import contextlib
+from typing import Callable, Tuple
 
 import joblib
+from tqdm.auto import tqdm
 
 from ambrosia import types
+from ambrosia.tools.decorators import tqdm_parallel_decorator
 
 
 @contextlib.contextmanager
@@ -26,6 +29,23 @@ def tqdm_joblib(tqdm_object):
     finally:
         joblib.parallel.BatchCompletionCallBack = old_batch_callback
         tqdm_object.close()
+
+
+def handle_bootstrap_multiprocessing(
+    n_jobs: int, criterion: int, func: Callable, desc: str, total: int, **kwargs
+) -> Tuple[int, int]:
+    """
+    Handle parameters for bootstrap parallelism of experiment design computation tasks.
+    """
+    progress_bar = tqdm(desc=desc, total=total)
+    if criterion == "bootstrap":
+        bootstrap_n_jobs, n_jobs = n_jobs, 1
+        func = tqdm_parallel_decorator(func)
+        kwargs["progress_bar"] = progress_bar
+    else:
+        bootstrap_n_jobs: int = 1
+        progress_bar = tqdm_joblib(progress_bar)
+    return n_jobs, bootstrap_n_jobs, func, progress_bar, kwargs
 
 
 def wrap_cols(cols: types.ColumnNamesType) -> types.ColumnNamesType:
